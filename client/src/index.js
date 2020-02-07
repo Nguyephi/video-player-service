39c484782b47819e569801e6534f6317d576d3c7
Phil <pronewgen92@gmail.com>
2020-02-07 09:05:26 -0500

    Avoid proxy conflict. Change app id to video-player-service.

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App streamerId={window.location.search.slice(1)} />, document.getElementById('app'));
+ReactDOM.render(<App streamerId={window.location.search.slice(1)} />, document.getElementById('video-player-service'));
